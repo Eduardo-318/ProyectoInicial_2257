8679f80754391332b9f4b24781411aedcc4f44bc
Eduardo_Gutierrez <eduardogutierrez318@aragon.unam.mx>
2021-03-12 17:36:52 -0600

    Cuestionario 8. Actividad en clase 12-mar-21

--- src/mx/unam/aragon/fes/gui/AltaEmpleado.java
+++ src/mx/unam/aragon/fes/gui/AltaEmpleado.java
@@ -12,20 +12,22 @@ import mx.unam.aragon.fes.Direccion;
 import mx.unam.aragon.fes.Empleado;
 import mx.unam.aragon.fes.persistencia.ArchivoEmpleado;
 
-
-
 /**
  *
  * @author unam
  */
 public class AltaEmpleado extends javax.swing.JFrame {
-private ArrayList<Empleado> lista;
+
+    private ArrayList<Empleado> lista;
+    private int indice;
+
     /**
      * Creates new form AltaEmpleado
      */
     public AltaEmpleado() {
         initComponents();
-    lista= new ArrayList<Empleado>();   
+        lista = new ArrayList<Empleado>();
+        indice = 0;
     }
 
     /**
@@ -75,6 +77,7 @@ private ArrayList<Empleado> lista;
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
+        jLabel14 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -310,8 +313,18 @@ private ArrayList<Empleado> lista;
         });
 
         jButton2.setText("Cargar");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
 
         jButton3.setText(">>");
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
+            }
+        });
 
         jButton4.setText("<<");
 
@@ -330,6 +343,9 @@ private ArrayList<Empleado> lista;
             }
         });
 
+        jLabel14.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel14.setText("0");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -342,22 +358,28 @@ private ArrayList<Empleado> lista;
                         .addComponent(jButton5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton6)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(189, 189, 189)
                         .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(177, 177, 177)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(438, 438, 438)
+                .addComponent(jLabel14, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addComponent(jLabel14)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2)
@@ -375,21 +397,20 @@ private ArrayList<Empleado> lista;
         // TODO add your handling code here:
         System.out.println("Nuevo empleado!!!");
         this.limpiarFormulario();
-        this.jButton6.setEnabled(true); 
-        
-       
-        
+        this.jButton6.setEnabled(true);
+
+
     }//GEN-LAST:event_jButton5MouseClicked
 
     private void jButton6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton6MouseClicked
         // TODO add your handling code here:
-                
-         Empleado emp= new Empleado();
-        emp.setNombre( this.jTextField1.getText() );
-        emp.setApPaterno(this.jTextField2.getText() );
-        emp.setApMaterno( jTextField3.getText() );
-        emp.setEdad( Integer.parseInt( jTextField4.getText()  )  );  // converte a int
-        emp.setCurp( jTextField5.getText());
+
+        Empleado emp = new Empleado();
+        emp.setNombre(this.jTextField1.getText());
+        emp.setApPaterno(this.jTextField2.getText());
+        emp.setApMaterno(jTextField3.getText());
+        emp.setEdad(Integer.parseInt(jTextField4.getText()));  // converte a int
+        emp.setCurp(jTextField5.getText());
         emp.setDomicilio(new Direccion());
         emp.getDomicilio().setCalle(this.jTextField6.getText());
         emp.getDomicilio().setNumero(this.jTextField7.getText());
@@ -402,29 +423,29 @@ private ArrayList<Empleado> lista;
         emp.setSueldo(Float.parseFloat(this.jTextField13.getText()));
         emp.setHorasExtra(Integer.parseInt(this.jTextField14.getText()));
         int confirmar = JOptionPane.showConfirmDialog(this, "¿Deseas guardar estos datos?");
-    //subirlo a memoria
-        System.out.println("boton"+confirmar);
-        if (confirmar==0){
-    lista.add(emp);
-        }else{
-        JOptionPane.showMessageDialog(this, "¡¡¡No se guardo el empleado!!!");
+        //subirlo a memoria
+        System.out.println("boton" + confirmar);
+        if (confirmar == 0) {
+            lista.add(emp);
+        } else {
+            JOptionPane.showMessageDialog(this, "¡¡¡No se guardo el empleado!!!");
         }
-        
-        System.out.println("Empleado en el ARREGLO= "+ lista.size());
+
+        System.out.println("Empleado en el ARREGLO= " + lista.size());
         limpiarFormulario();
         jButton6.setEnabled(false);
-        
-        
+
+
     }//GEN-LAST:event_jButton6MouseClicked
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
         // TODO add your handling code here:
-        ArchivoEmpleado persistencia= new ArchivoEmpleado();
-        
-        JFileChooser jfc= new JFileChooser();
+        ArchivoEmpleado persistencia = new ArchivoEmpleado();
+
+        JFileChooser jfc = new JFileChooser();
         jfc.showSaveDialog(this);
-        String archivo=jfc.getSelectedFile().getAbsolutePath();
-        System.out.println("La ruta seleccionada es: " +archivo);
+        String archivo = jfc.getSelectedFile().getAbsolutePath();
+        System.out.println("La ruta seleccionada es: " + archivo);
         persistencia.setArchivo(archivo);
         try {
             persistencia.guardarEmpleados(lista);
@@ -433,31 +454,74 @@ private ArrayList<Empleado> lista;
         }
     }//GEN-LAST:event_jButton1MouseClicked
 
-     
-    private void limpiarFormulario(){
-      this.jTextField1.setText("");
-      this.jTextField2.setText("");
-      this.jTextField3.setText("");
-      this.jTextField4.setText("0");
-      this.jTextField5.setText("");
-      this.jTextField6.setText("");
-      this.jTextField7.setText("");
-      this.jTextField8.setText("");
-      this.jTextField9.setText("");
-      this.jTextField10.setText("");
-      this.jTextField10.setColumns(10);
-      this.jTextField11.setText("0");
-      this.jTextField11.setColumns(10);
-      this.jTextField12.setText("");
-      this.jTextField13.setText("0.0");
-      this.jTextField14.setText("0");
-      this.jTextField15.setText("");
-      
-      
-      
-      
-    }
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        // TODO add your handling code here:
+        int respuesta = JOptionPane.showConfirmDialog(this,
+                 "Esta acción borrara los datos actuales. ¿Estas seguro de continuar?");
+        if (respuesta == 0) {//Constantes de clases
+            //leer del archivo
+            //subir el array list
+            //el primer elemento del array list se pondra en el formulario
+            ArchivoEmpleado persistencia = new ArchivoEmpleado();
+            JFileChooser jfc = new JFileChooser();
+            jfc.showOpenDialog(this);
+            persistencia.setArchivo(jfc.getSelectedFile().getAbsolutePath());
+            this.lista = persistencia.leerEmpleados();
+            this.limpiarFormulario();
+            this.indice = 0;
+            this.llenarFormulario(this.indice);
+        } else {
+
+        }
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+        if (this.indice < this.lista.size()-1) {
+            this.indice++;
+            this.jLabel14.setText("" + this.indice);
+            llenarFormulario(indice);
+        }else{
+            this.indice=0;
+        }
+        this.jLabel14.setText(""+this.indice);
+        llenarFormulario(indice);
+    }//GEN-LAST:event_jButton3MouseClicked
+
     
+
+    private void llenarFormulario(int indice) {
+        if (lista.size() > 0) {
+            Empleado e = lista.get(indice);
+            this.jTextField1.setText(e.getNombre());
+            this.jTextField2.setText(e.getApPaterno());
+            this.jTextField3.setText(e.getApMaterno());
+            this.jTextField4.setText("" + e.getEdad());
+            this.jTextField5.setText(e.getCurp());
+
+        }
+    }
+
+    private void limpiarFormulario() {
+        this.jTextField1.setText("");
+        this.jTextField2.setText("");
+        this.jTextField3.setText("");
+        this.jTextField4.setText("0");
+        this.jTextField5.setText("");
+        this.jTextField6.setText("");
+        this.jTextField7.setText("");
+        this.jTextField8.setText("");
+        this.jTextField9.setText("");
+        this.jTextField10.setText("");
+        this.jTextField10.setColumns(10);
+        this.jTextField11.setText("0");
+        this.jTextField11.setColumns(10);
+        this.jTextField12.setText("");
+        this.jTextField13.setText("0.0");
+        this.jTextField14.setText("0");
+        this.jTextField15.setText("");
+
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -505,6 +569,7 @@ private ArrayList<Empleado> lista;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
